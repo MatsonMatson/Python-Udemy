1b232c46bbb7060a55811912d58fd09a7ce66108
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-29 17:21:02 -0300

    Add files via upload

---
+++ aula1.py
@@ -0,0 +1,20 @@
+"""
+aspas duplas serve para 
+DocString 
+escreve oq eu
+quiser
+
+
+
+"""
+'''
+aspas duplas usa para escrever notas
+sadasd
+adsdadsa
+asdad
+'''
+
+#comentario em linha unica
+print(123) #comentario na frente
+# baixo
+print(456)
