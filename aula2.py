d01d56e05ef5a7c106513f6078d88bb6d3458aa9
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-29 17:21:18 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,7 @@
+# sep ele separa os itens e pode colocar algo que você queira, como por ex pode printar algo dentro 
+#\n = quebra de linha ou Line Feed -> LF
+#\r\n = CRLF
+print(12, 305, 1265, sep='-')
+print(65, 88, sep='-')
+#outro exemplo
+print('macaco', 'banana', sep=' come ')
