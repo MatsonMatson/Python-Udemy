29aa609ecfbdd24863398f05d7985eb0a2ef8df0
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-29 18:36:14 -0300

    Add files via upload

---
+++ aula7.py
@@ -0,0 +1,15 @@
+"""
+Váriaveis são usadas para salvar algo na memória do computador.
+PEP8: inicie variavéis com letras minúsculas, pode usar
+números e underline _.
+O sinal de = é utilizado para atribuição de valores.
+atribuir um valor a um nome(variável).
+Uso: nome_variavel = expressão 
+"""
+nome = 'Matso'
+idade = 20
+maior_de_idade = idade >= 18
+print('-' * 10)
+print('Nome:', nome, 'Idade:', idade)
+print('É maior?: ', maior_de_idade)
+print('-' * 10)
