207dbfed12c6198352a42cdd7acd22b7d31c4980
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-29 17:21:36 -0300

    Add files via upload

---
+++ aula4.py
@@ -0,0 +1,37 @@
+#Tipos int e float
+
+"""
+int -> Númeors inteiros
+O tipo int representa qualquer número
+positivo ou negativo. int sem sinal é considerado positivo 
+"""
+print('-' * 10)
+print('\033[31mint\033[m')
+print(11)#int
+print(-11)#int
+print(0)#int
+print('-' * 10)
+"""
+flota -> Número com ponto flutuante
+o tipo float representa qualquer número
+positivo ou negativo com ponto flutuante 
+float sem sinal é considerado positivo 
+"""
+print('-' * 10)
+print('\033[35mfloat\033[m')
+print(1.1)#float
+print(10,5)#float
+print(-1.75)#float
+print('-' * 10)
+"""
+type -> mostra o tipo que o Python inferiu ao valor
+type não é uma função e sim uma classe 
+"""
+print('-' * 10)
+print('\033[33mclass type\033[m')
+print(type('Matso'))
+print(type(5))
+print(type(0))
+print(type(1.1))
+print(type(-1.1))
+print('-' * 10)
