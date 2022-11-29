78a935e3ff0b11651e21b5485df07edc2e3ebeaf
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-29 17:21:28 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,25 @@
+"""
+DocString
+Python = Linguagem de progamação
+Tipo de tipagem = Dinâmica / forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas ''
+"""
+
+print(1234)
+
+# Aspas Simples
+print('Matso the Monkey')
+print(1, 'Matso "the" Monkey')
+
+# Aspas Duplas
+print("Matso the Monkey")
+print(2, "Matso 'the' Monkey")
+
+# Escape 
+print("Matso \"Monkey\"")
+
+# r
+print(r"Matso \"Monkey\"")
+
+
