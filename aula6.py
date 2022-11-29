ee08652e85a4d2ee3d09826dd3dd1bfef459c2c6
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-29 17:46:48 -0300

    Add files via upload

---
+++ aula6.py
@@ -0,0 +1,13 @@
+#Conversão de tipos e coerção
+"""
+type convertion, typecasting, coercion
+é o ato de converter um tipo do outro 
+Tipos imutáveis e primitivos:
+str, int, float e bool.
+"""
+print('-' * 10)
+print(int('1'), type(int('1')))
+print(type(float('1') + 1))
+print(bool(' '))
+print(str(11) + 'b')
+print('-' * 10)
