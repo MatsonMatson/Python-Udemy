7886bbd068ea5043cd6170ef4e48bac769d99fd6
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-30 15:43:47 -0300

    Add files via upload

---
+++ aula8.py
@@ -0,0 +1,21 @@
+adicao = 10 + 10
+print('Adição =', adicao)
+
+subtracao = 10 - 5
+print('Subtração =', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação =', multiplicacao)
+
+divisao = 10 / 2 # float
+print('Divisão =', divisao)
+
+divisaoint = 10 // 2
+print('Divisão Inteira =', divisaoint)
+
+expoenciacao = 10 ** 2
+print('Exponenciação =', expoenciacao)
+
+modulo = 10 % 2 # resto da divisão
+print('Módulo =', modulo)
+
