c0fa728c0de080fe5768acec44e6111110bb6ebb
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-11-30 15:44:01 -0300

    Add files via upload

---
+++ aula9.py
@@ -0,0 +1,16 @@
+import emoji
+concatenacao = 'matso' + ' ' + 'monkey'
+print(concatenacao)
+
+print('-' * 10)
+
+a_dez_vezes = 'A' * 10
+print(a_dez_vezes)
+
+print('-' * 10)
+
+tres_vezes_macaco = ' Macaco ' * 3
+print(tres_vezes_macaco)
+
+print('-' * 10)
+print(emoji.emojize(':fox:'))
