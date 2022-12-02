663a3bf894b7526979e95789050fc7bedc171a34
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-12-02 13:59:01 -0300

    Add files via upload

---
+++ aula15.py
@@ -0,0 +1,18 @@
+"""
+Operadores de comparação (RELACIONAIS)
+OP     Significado     Exemplo (True)
+>      maior           2 > 1
+>=     maior igual     2 >= 2
+<      menor           1 < 2
+<=     menor igual     2 <= 2
+==     igual          'a' == 'b'
+!=     diferente      'a' != 'b'
+"""
+
+maior = 2 > 1
+maiorigual = 2 > 2
+menor = 1 < 2
+menorigual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' == 'b'
+
