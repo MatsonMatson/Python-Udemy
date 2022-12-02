0a889b3315bc250ca00dccbed91e1f8bce5e9010
Matheus Favara <99259207+MatsonMatson@users.noreply.github.com>
2022-12-02 13:58:09 -0300

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,10 @@
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+
+if num1 > num2:
+    print(f'O primeiro valor {num1} é maior que o segundo valor {num2}')
+elif num1 == num2:
+    print('Os valores são identicos')
+else:
+    print(f'O segundo valor {num2} é maior que o primeiro valor {num1}')
+       
